95828d40bbcd2ec38faeafe47622431bc30d8f47
Давыдов Павел <davidov.pavel@gmail.com>
2017-09-17 21:25:42 +0300

    add

---
+++ d3js/src/main.js
@@ -0,0 +1,6 @@
+﻿
+import * as d3 from 'd3';
+
+const chart = d3.select('body')
+    .append('svg')
+    .attr('id', 'chart');
---
+++ d3js/webpack.config.js
@@ -0,0 +1,45 @@
+﻿"use strict";
+
+const path = require('path');
+
+module.exports = {
+
+    entry: "./src/main.js",
+
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: "bundle.js"
+    },
+
+    devServer: {
+        contentBase: ".",
+        host: "localhost",
+        port: 9000
+    },
+
+    devtool: 'inline-source-map',
+
+    module: {
+
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2017']
+                    }
+                }
+            },
+            {
+                test: /\.json$/,
+                loader: 'json-loader'
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    }
+};
